f287fbd18121aac3068a5c2279bb595d0da1e420
iamwebwiz <iamwebwiz@gmail.com>
2018-10-31 02:05:39 +0100

    update movie controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    /**
+     * display all movies
+     *
+     * @return Response
+     */
+    public function showMovies()
+    {
+        return Movie::all();
+    }
 }
