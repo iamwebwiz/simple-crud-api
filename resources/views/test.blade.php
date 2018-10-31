3f293df549b14443126b6c171a94bbf05c429203
iamwebwiz <iamwebwiz@gmail.com>
2018-10-31 05:07:08 +0100

    update test view

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,7 +8,7 @@
                     <div class="card-header">{{ __('Add Movie') }}</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('api.movies.store') }}" enctype="multipart/form-data">
+                        <form method="POST" action="{{ url('/api/movies/new') }}" enctype="multipart/form-data">
                             @csrf
 
                             <div class="form-group">
