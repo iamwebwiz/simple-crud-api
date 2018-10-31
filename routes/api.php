a591e8bda96ab79cefa2db68a1fd2da8002656bd
iamwebwiz <iamwebwiz@gmail.com>
2018-10-31 02:38:39 +0100

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/movies', 'MovieController@showMovies');
+
+Route::get('/movies/{movie}', 'MovieController@showMovieInformation');
