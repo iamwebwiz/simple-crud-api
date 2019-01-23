3da6b9b3037efcec86f03be2dc9aa92dd9c3abfe
Ezekiel Oladejo <iamwebwiz@gmail.com>
2019-01-23 15:43:37 +0100

    update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (config('app.env') !== 'local')
+            URL::forceScheme('https');
     }
 
     /**
